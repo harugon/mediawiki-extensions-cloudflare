bc52d13aed514dd448435fe83f8005517cbe5f63
harugon <info@harugon.com>
2023-10-28 22:31:20 +0900

    Add check to ensure urls array is not empty before purging cache

--- includes/EventRelayer.php
+++ includes/EventRelayer.php
@@ -5,55 +5,62 @@ use MediaWiki\MediaWikiServices;
 
 class EventRelayer extends \EventRelayer {
 
-	/**
-	 * @var CloudflareAPIRequester
-	 */
-	private $cloudflareAPIRequester;
-	private $config;
+    /**
+     * @var CloudflareAPIRequester
+     */
+    private $cloudflareAPIRequester;
+    private $config;
 
-	public function __construct( array $params ) {
-		parent::__construct( $params );
-		$this->cloudflareAPIRequester = MediaWikiServices::getInstance()->getService( 'CloudflareAPIRequester' );
-		$this->config = MediaWikiServices::getInstance()->getMainConfig();
-	}
+    public function __construct( array $params ) {
+        parent::__construct( $params );
+        $this->cloudflareAPIRequester = MediaWikiServices::getInstance()->getService( 'CloudflareAPIRequester' );
+        $this->config = MediaWikiServices::getInstance()->getMainConfig();
+    }
 
-	/**
-	 * @param string $channel
-	 * @param array $events List of event data maps
-	 * @return bool Success
-	 * @throws \MWException
-	 */
-	protected function doNotify( $channel, array $events ): bool {
-		if ( $channel === 'cdn-url-purges' ) {
-			$uploadPath = $this->config->get( 'UploadPath' );
+    /**
+     * @param string $channel
+     * @param array $events List of event data maps
+     * @return bool Success
+     * @throws \MWException
+     */
+    protected function doNotify( $channel, array $events ): bool {
+        if ( $channel === 'cdn-url-purges' ) {
+            $uploadPath = $this->config->get( 'UploadPath' );
 
-			$files = [];
-			$articles = [];
-			foreach ( $events as $event ) {
-				/** @var $event array['urls' => $url,'timestamp' => $ts] */
-				$url = $this->expandURL( $event['url'] );
-				$isFileURL = strpos( $url, $uploadPath ) !== false;
-				if ( $isFileURL ) {
-					$files[] = $url;
-				} else {
-					$articles[] = $url;
-				}
-			}
+            $files = [];
+            $articles = [];
+            foreach ( $events as $event ) {
+                /** @var $event array['urls' => $url,'timestamp' => $ts] */
+                $url = $this->expandURL( $event['url'] );
+                $isFileURL = strpos( $url, $uploadPath ) !== false;
+                if ( $isFileURL ) {
+                    $files[] = $url;
+                } else {
+                    $articles[] = $url;
+                }
+            }
 
-			if ( $this->config->get( 'CloudflarePurgePage' ) && $this->config->get( 'CloudflarePurgeFile' ) ) {
-				$allUrls = array_merge( $articles, $files );
-				$this->cloudflareAPIRequester->cachePurge( $allUrls );
-			} elseif ( $this->config->get( 'CloudflarePurgePage' ) ) {
-				$this->cloudflareAPIRequester->cachePurge( $articles );
-			} elseif ( $this->config->get( 'CloudflarePurgeFile' ) ) {
-				$this->cloudflareAPIRequester->cachePurge( $files );
-			}
+            if ( $this->config->get( 'CloudflarePurgePage' ) && $this->config->get( 'CloudflarePurgeFile' ) ) {
+                $allUrls = array_merge( $articles, $files );
 
-		}
-		return true;
-	}
+                if (count($allUrls)>0) {
+                    $this->cloudflareAPIRequester->cachePurge( $allUrls );
+                }
+            } elseif ( $this->config->get( 'CloudflarePurgePage' ) ) {
 
-	private function expandURL( $url ): string {
-		return (string)MediaWikiServices::getInstance()->getUrlUtils()->expand( $url, PROTO_INTERNAL );
-	}
+                if (count($articles)>0) {
+                    $this->cloudflareAPIRequester->cachePurge( $articles );
+                }
+            } elseif ( $this->config->get( 'CloudflarePurgeFile' ) )
+
+                if (count($files)>0) {
+                    $this->cloudflareAPIRequester->cachePurge( $files );
+                }
+        }
+        return true;
+    }
+
+    private function expandURL( $url ): string {
+        return (string)MediaWikiServices::getInstance()->getUrlUtils()->expand( $url, PROTO_INTERNAL );
+    }
 }
